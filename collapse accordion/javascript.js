81621ee4ed782f9938740a41f7915a5f2c555a2a
Omid Sadeghi <omid69sdgi@gmail.com>
2025-01-20 21:57:08 +0330

    Update javascript.js

--- collapse accordion/javascript.js
+++ collapse accordion/javascript.js
@@ -1,14 +1,11 @@
 $(document).ready(function(){
-    // عملکرد برای هر گروه آکاردئون به طور مستقل
     $(".accordion-header").click(function(){
-        var group = $(this).closest(".accordion-group"); // گروه مربوطه
-        var content = $(this).next(".accordion-content"); // محتوای مربوطه
+        var group = $(this).closest(".accordion-group");
+        var content = $(this).next(".accordion-content");
         var isVisible = content.is(":visible");
 
-        // بستن تمام محتواهای این گروه
-        group.find(".accordion-content").slideUp().prev(".accordion-header").removeClass("active");
-
-        // اگر محتوا باز نبوده، آن را باز کن
+          group.find(".accordion-content").slideUp().prev(".accordion-header").removeClass("active");
+       
         if (!isVisible) {
             content.slideDown();
             $(this).addClass("active");
