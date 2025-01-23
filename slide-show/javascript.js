e8474349633f33ca9af01186cc53a77026ad66ec
omidsdgi <omid69sdgi@gmail.com>
2025-01-23 13:40:13 +0330

    Add JavaScript file for the page

---
+++ slide-show/javascript.js
@@ -0,0 +1,40 @@
+const slider = document.getElementById('slider');
+const images = document.querySelectorAll('img');
+const nextButton = document.getElementById('next');
+const prevButton = document.getElementById('prev');
+
+let currentIndex = 0;
+let autoPlayInterval;
+
+function showSlide(index) {
+    images.forEach((img,i) => {
+        img.classList.toggle("active", i===index);
+
+    })
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showSlide(currentIndex);
+}
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showSlide(currentIndex);
+}
+
+function startAutoPlay() {
+    autoPlayInterval = setInterval(nextSlide,5000)
+}
+function stopAutoPlay() {
+    clearInterval(autoPlayInterval);
+}
+nextButton.addEventListener('click', ()=>{
+    nextSlide();
+});
+prevButton.addEventListener('click', ()=>{
+    prevSlide();
+})
+slider.addEventListener('mouseenter', stopAutoPlay)
+slider.addEventListener('mouseleave', startAutoPlay)
+
+startAutoPlay();
